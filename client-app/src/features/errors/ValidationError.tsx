b124c29c0aed417f8b3a7f9526d71ad92041f774
Jason Coley <jason@software-solutions.co.nz>
2023-03-15 11:07:58 +1000

    Error Handling added  (API & client)

---
+++ client-app/src/features/errors/ValidationError.tsx
@@ -0,0 +1,20 @@
+import { Message, MessageItem, MessageList } from "semantic-ui-react";
+
+interface Props {
+	errors: string[];
+}
+
+const ValidationError = ({ errors }: Props) => {
+	return (
+		<Message error>
+			{errors && (
+				<MessageList>
+					{errors.map((err: string, i) => (
+						<MessageItem key={i}>{err}</MessageItem>
+					))}
+				</MessageList>
+			)}
+		</Message>
+	);
+};
+export default ValidationError;
