c43007b7b6f3e6de8cef6f5849974f42ba17e40f
Jason Coley <jason@software-solutions.co.nz>
2023-03-29 10:25:02 +1000

    Changes to PostGres connstring for fly.io

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -28,9 +28,11 @@ namespace API.Controllers
         {
             if (result == null) return NotFound();
             if (result.IsSuccess && result.Value != null)
+            {
                 Response.AddPaginationHeader(result.Value.CurrentPage,
                     result.Value.PageSize, result.Value.TotalCount, result.Value.TotalPages);
-            return Ok(result.Value);
+                return Ok(result.Value);
+            }
             if (result.IsSuccess && result.Value == null)
                 return NotFound();
             return BadRequest(result.Error);
--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -35,7 +35,6 @@ namespace API.Extensions
                 {
                     // Use connection string provided at runtime by FlyIO.
                     var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
-
                     // Parse connection URL to connection string for Npgsql
                     connUrl = connUrl.Replace("postgres://", string.Empty);
                     var pgUserPass = connUrl.Split("@")[0];
@@ -46,12 +45,14 @@ namespace API.Extensions
                     var pgPass = pgUserPass.Split(":")[1];
                     var pgHost = pgHostPort.Split(":")[0];
                     var pgPort = pgHostPort.Split(":")[1];
+                    var updatedHost = pgHost.Replace("flycast", "internal");
 
-                    connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
+                    connStr = $"Server={updatedHost}; Port={pgPort}; User Id = {pgUser}; Password ={pgPass}; Database ={pgDb};";
                 }
 
                 // Whether the connection string came from the local development configuration file
                 // or from the environment variable from FlyIO, use it to set up your DbContext.
+                Console.WriteLine(connStr);
                 options.UseNpgsql(connStr);
             });
 
